8936422a9a9e7174110eb05cab1d7d43134eec0e
julietzhu <724434852@qq.com>
2014-07-24 09:22:13 +0800

    样式已修改，手机端可使用

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,15 +11,23 @@ angular
   ])
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+        .when('/', {
+            templateUrl: 'views/main.html',
+            controller: 'MainController'
+        })
+        .when('/create_activity', {
+            templateUrl: 'views/create_activity.html',
+            controller: 'CreateActivityController'
+        })
+        .when('/activity_sign_up', {
+            templateUrl: 'views/activity_sign_up.html',
+            controller: 'ActivitySignUpController'
+        })
+        .when('/activity_list', {
+            templateUrl: 'views/activity_list.html',
+            controller: 'ActivityListController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
   });
