eadc6bb6d1bdd8ce86cab25d471a7e411f0b7563
julietzhu <724434852@qq.com>
2014-07-26 10:54:13 +0800

    重写开始和结束按钮

--- app/scripts/controllers/ActivitySignUpController.js
+++ app/scripts/controllers/ActivitySignUpController.js
@@ -17,21 +17,38 @@ angular.module('partyBidApp')
 
         //找到了活动的key
         var activity_key = localStorage.getItem('activity_key');
-        var activities = Number(activity_key);//将对象变成string
+        var activities = Number(activity_key);//将对象变成number
         var activity = [];
         activity = JSON.parse(localStorage[activities]);
-     //   activity = localStorage.getItem(activity_key);
         localStorage.removeItem("activity_key");
 
-        //活动状态
+        $scope.state = "开始";
+        var i = 0;
+        $scope.start_finish = function(){     //开始按钮变成结束按钮
+            if(i%2==0)                        //目前是开始状态
+            {
+                $scope.state = "结束";
+                i++;
+            }
+            else{
+                $scope.state = "开始";
+                i++;
+            }
+        }
+        i=0;
 
+ /*       var t ;
         //开始按钮和结束按钮互斥显示
         $scope.start = true;
         $scope.start_finish = function(){     //开始按钮变成结束按钮
             $scope.start = false;
             activity[2] = "start";
             localStorage[activities] = JSON.stringify(activity);
+            t = true;    //判断是否有活动进行，true为有活动
         }
+
+        //开始按钮是否可用
+        $scope.xx = t;
 
         $scope.finish_start = function(){     //结束按钮变成开始按钮，并弹出提示框
             if(confirm('确认要结束本次报名吗？'))
@@ -39,7 +56,8 @@ angular.module('partyBidApp')
                 $scope.start = true;
                 activity[2] = "finish";
                 localStorage[activities] = JSON.stringify(activity);
+          //      flag = false;
             }
             $scope.activity_state = "start";
-        }
+        }*/
     });
